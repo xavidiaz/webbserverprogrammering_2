76732c41153560e5eb824af7f06ae48ca1778c2d
Javier <me@xavidiaz.com>
2020-09-01 18:05:30 +0200

    Begin

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/xavidiaz/.bitnami/stackman/machines/xampp/volumes/root/htdocs/Webbserverprogrammering-2/Lanesystemet/webbserverprogrammering_2/.cache/dev-404-page.js"))),
+  "component---src-pages-counter-js": hot(preferDefault(require("/Users/xavidiaz/.bitnami/stackman/machines/xampp/volumes/root/htdocs/Webbserverprogrammering-2/Lanesystemet/webbserverprogrammering_2/src/pages/counter.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/xavidiaz/.bitnami/stackman/machines/xampp/volumes/root/htdocs/Webbserverprogrammering-2/Lanesystemet/webbserverprogrammering_2/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/xavidiaz/.bitnami/stackman/machines/xampp/volumes/root/htdocs/Webbserverprogrammering-2/Lanesystemet/webbserverprogrammering_2/src/pages/page-2.js")))
+}
+
