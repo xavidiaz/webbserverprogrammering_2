f1e71aaa523b416789bf64438f1794bb6843f9b4
Javier <me@xavidiaz.com>
2020-09-02 08:14:24 +0200

    First Template

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('template.class.php');
 define('TEMPLATE_PATH', 'templates');
 
 //Instanciate new object
-$template = new Template();
+$template = new Template(TEMPLATE_PATH.'/test.tpl.html');
 
 //Assign values
 
--- template.class.php
+++ template.class.php
@@ -4,8 +4,15 @@ class Template{
     var $assignedValues = array();
     var $tpl;
 
-    function __construct(){
-echo "Hello World!";
+    function __construct($_path = ''){
+        if(!empty($_path)){
+            if(file_exists($_path)){
+                $this->tpl=file_get_contents($_path);
+            }else{
+                echo "<b>Template Error: </b>File Inclusion Error.";
+            }
+        }
+        echo $this->tpl;
     }
     
 
