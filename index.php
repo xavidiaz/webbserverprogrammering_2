6f359460c3acc314230889736d0fcdad81b2ea08
Javier <me@xavidiaz.com>
2020-09-02 07:54:09 +0200

    First Hello World!

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//Setting up important stuff
+require_once('template.class.php');
+define('TEMPLATE_PATH', 'templates');
+
+//Instanciate new object
+$template = new Template();
+
+//Assign values
+
+
+//Showing content
---
+++ template.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class Template{
+    var $assignedValues = array();
+    var $tpl;
+
+    function __construct(){
+echo "Hello World!";
+    }
+    
+
+    function assign(){
+
+    }
+
+    function show(){
+
+
+    }
+}
