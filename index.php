7a44232ac85cfd791734f51ffcc116e2b5d3ae4d
Javier <me@xavidiaz.com>
2020-09-02 12:11:32 +0200

    Start point

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+###
+// Create a product list
+$products = [
+    [
+        'name'          => 'Notebook',
+        'description'   => 'Core i7',
+        'value'         =>  800.00,
+        'date_register' => '2017-06-22',
+    ],
+    [
+        'name'          => 'Mouse',
+        'description'   => 'Razer',
+        'value'         =>  125.00,
+        'date_register' => '2017-10-25',
+    ],
+    [
+        'name'          => 'Keyboard',
+        'description'   => 'Mechanical Keyboard',
+        'value'         =>  250.00,
+        'date_register' => '2017-06-23',
+    ],
+];
+###
+
+// RENDER
+echo $twig->render('first.html.twig', ['name' => 'John Doe', 
+    'occupation' => 'gardener']);
+
+echo $twig->render('products.html.twig', ['products' => $products] );
