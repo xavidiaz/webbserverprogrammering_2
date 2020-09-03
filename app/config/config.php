d6e402ebbfd3c5d26c66f07b2d07221d739aed94
Javier <me@xavidiaz.com>
2020-09-03 16:14:01 +0200

    constants path ADDED (root, url, sitename)

--- app/config/config.php
+++ app/config/config.php
@@ -1,2 +1,15 @@
 <?php
-Echo 'This is config.php';
+// DATEBASE
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASW', '');
+define('DB_NAME', '');
+
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT
+define('URLROOT', 'http://localhost:8080/Webbserverprogrammering-2/Lanesystemet/webbserverprogrammering_2/');
+
+//SITENAME
+define('SITENAME', 'Lånesystem');
--- app/require.php
+++ app/require.php
@@ -4,5 +4,7 @@ require_once 'libraries/Kernel.php';
 require_once 'libraries/Controller.php';
 require_once 'libraries/Database.php';
 
+require_once 'config/config.php';
+
 // Instantiate core class
 $init = new Core();
